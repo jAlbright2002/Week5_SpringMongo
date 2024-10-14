dbdf1e8e4e6a61e07a3bb8576c620bcc4dcd9e3f
James A <G00379271@atu.ie>
2024-10-14 17:23:54 +0100

    Add exception handlers for invalid properties in customer and order classes and add Controller Advice class

--- src/main/java/ie/atu/week5/customerapp/Customer.java
+++ src/main/java/ie/atu/week5/customerapp/Customer.java
@@ -1,5 +1,7 @@
 package ie.atu.week5.customerapp;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -10,6 +12,8 @@ public class Customer {
 
     @Id
     private String id;
+    @NotBlank(message = "Name must not be empty")
     private String name;
+    @Email(message = "Email must be valid with @ included")
     private String email;
 }
